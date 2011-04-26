888562dbd08f4ff0bc7fc5cb813b9930a274a7d7
Greg Pascale <greg.pascale@gmail.com>
2011-04-26 02:12:36 -0700

    actually commiting changes

--- app.js
+++ app.js
@@ -11,12 +11,28 @@ app.get('/', function(req, res) {
     res.render('index.html', { layout: false });
 });
 
+var data = [
+    { text: "I like Coldplay. AIAD?", username: "Greg" },
+    { text: "I wear Ed Hardy. AIAD?", username: "Bob" }
+];
+
 app.post('/sendtext', function(req, res) {
     console.log(req.body);
     if (req.body) {
-	console.log('received \'' + req.body.thetext + '\''); 
-	res.send(req.body.thetext, 200);
+	console.log('received \'' + req.body.text + '\''); 
+        data.push({ text: req.body.text, username: req.body.username });
+        res.send("", 200);
     }
+    else {
+	res.send("No text!", 404);
+    }
+});
+
+app.get('/getdata', function(req, res) {
+    console.log('getdata');
+    console.log('foo');
+        
+    res.send({ error: false, data: data});
 });
 
 app.listen(3000);
